8f1e7ebf89bdad358ac669c5cfeec6f702ea852b
taewoo <tary82504@berkeley.edu>
2020-09-28 11:02:34 -0700

    optimized sumof1darray solution from quadratic to linear

--- problems/sumOfArray.js
+++ problems/sumOfArray.js
@@ -1,3 +1,23 @@
+
+//optimized
+const sumOfArray = (inputArray) => {
+  const resultArray = [];
+  for (let i = 0; i < inputArray.length; i ++) {
+    if (i === 0) { 
+      resultArray[i] = inputArray[i];
+    } else {
+      resultArray[i] = resultArray[i-1] + inputArray[i];
+    }
+  }
+  return resultArray;
+}
+
+
+//one-liner
+//const sumOfArray = (inputArray) => (inputArray.map((val,index) => (inputArray.slice(0, index + 1).reduce((memo,val) => (memo + val)))));
+
+
+/* initial
 const sumOfArray = (inputArray) => {
   return inputArray.map((val, index) => {
     return localSum(inputArray.slice(0, index + 1));
@@ -7,5 +27,6 @@ const sumOfArray = (inputArray) => {
 const localSum = (array) => {
   return array.reduce((memo, val) => (memo + val));
 }
+*/
 
 module.exports = sumOfArray
